f90d186ee8338855cfd877cb3857fed1e211a40f
Loc Mai <locmai0201@gmail.com>
2023-01-14 14:46:41 +0700

    chore: add CHANGELOG everywhere
    Signed-off-by: Loc Mai <locmai0201@gmail.com>

--- archived/dendrite/files/create_db.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-for db in userapi_accounts userapi_devices mediaapi syncapi roomserver keyserver signingkeyserver federationapi appservice mscs; do
-    createdb -U dendrite -O dendrite dendrite_$db
-done
---
+++ charts/filebrowser/CHANGELOG.md
@@ -0,0 +1,4 @@
+# Change Log
+
+This file documents all notable changes to the Jenkins Helm Chart.
+The release numbering uses [semantic versioning](http://semver.org).
